d04471a0a97e077ac04b7f5b880193ade0c5e8bb
Filip Eller <filip.eller@aalto.fi>
2022-11-04 20:58:29 +0200

    Add title to education.

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,6 +4,7 @@ const Education = ({ items }: { items: any }) => {
   console.log({ items });
   return (
     <Paper sx={{ margin: 1, padding: 1 }}>
+      <Typography variant="h4">Education</Typography>
       {items &&
         items.map((item: any, index: number) => (
           <Paper key={index} sx={{ margin: 1, padding: 1 }}>
