c23048b0ad615c71f06a648d8f99c9122eeb7801
Filip Eller <filip.eller@aalto.fi>
2022-11-04 20:59:49 +0200

    Render list of items in work history.

--- src/services/profileInfo.ts
+++ src/services/profileInfo.ts
@@ -4,7 +4,10 @@ const getProfileInfo = async () => {
       { school: 'Aalto University', startDate: '2022-09-01' },
       { school: 'LUT University', startDate: '2022-10-01' },
     ],
-    workHistory: [{ companyName: 'Nitor', startDate: '2016-06-01' }],
+    workHistory: [
+      { companyName: 'Binance', startDate: '2016-06-01' },
+      { companyName: 'Junction', startDate: '2016-06-01' },
+    ],
   };
 };
 
