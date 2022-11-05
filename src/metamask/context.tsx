bee23d261e2eaa7add1bfd4ec4b9fb8baa8e5bc6
Filip Eller <filip.eller@aalto.fi>
2022-11-05 16:13:35 +0200

    Stop continuous Metamask context reloading.

--- src/metamask/context.tsx
+++ src/metamask/context.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
-import getBalance from './helpers/getBalance'
-import requestAccounts from './helpers/requestAccounts'
+import React, { useEffect } from 'react';
+import getBalance from './helpers/getBalance';
+import requestAccounts from './helpers/requestAccounts';
 
 /* interface Values {
   user: any
@@ -18,45 +18,48 @@ const initialValues: any = {
     balance: 0,
   },
   setUser: () => {},
-}
+};
 
-const MetamaskContext = React.createContext<any>(initialValues)
+const MetamaskContext = React.createContext<any>(initialValues);
 
 const MetamaskProvider = ({ children }: any) => {
-  const [contract, setContract] = React.useState<any>()
+  const [contract, setContract] = React.useState<any>();
   const [user, setUser] = React.useState<User>({
     address: window.ethereum?.selectedAddress,
     isConnected: false,
     balance: 0,
-  })
+  });
   const getUserInfo = async () => {
     if (window.ethereum) {
-      const userInfo = await requestAccounts()
+      const userInfo = await requestAccounts();
       setUser({
         ...user,
         ...userInfo,
-      })
+      });
     }
-  }
-  const values: any = { user, setUser, contract, setContract }
+  };
+  const values: any = { user, setUser, contract, setContract };
 
-  if (window.ethereum)
-    window.ethereum.on('accountsChanged', async (accounts: any) => {
-      getUserInfo()
-    })
+  // if (window.ethereum)
+  //   window.ethereum.on('accountsChanged', async (accounts: any) => {
+  //     getUserInfo();
+  //   });
 
   useEffect(() => {
+    getUserInfo();
     const timer = window.setInterval(() => {
-      getUserInfo()
-    }, 1500)
-    return () => clearInterval(timer)
-  }, [])
+      if (!user.isConnected) {
+        getUserInfo();
+      }
+    }, 15000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <MetamaskContext.Provider value={values}>
       {children}
     </MetamaskContext.Provider>
-  )
-}
+  );
+};
 
-export { MetamaskProvider, MetamaskContext }
+export { MetamaskProvider, MetamaskContext };
