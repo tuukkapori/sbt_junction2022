f21bedb2ce99dfa1e0f40cb09416f9ae3b1c6a0c
Filip Eller <filip.eller@aalto.fi>
2022-11-05 11:14:32 +0200

    Update token schema.

--- src/services/profileInfo.ts
+++ src/services/profileInfo.ts
@@ -11,12 +11,14 @@ const getProfileFromBlockchain = async (walletId: string) => {
   return {
     education: [
       {
+        type: 'education',
         school: 'Aalto University',
         degree: 'Bachelor of Computer Science',
         startDate: '2017-09-01',
         endDate: '2022-06-01',
       },
       {
+        type: 'education',
         school: 'LUT University',
         degree: 'Bachelor of Mathematics',
         startDate: '2013-09-01',
@@ -25,12 +27,14 @@ const getProfileFromBlockchain = async (walletId: string) => {
     ],
     workHistory: [
       {
+        type: 'work',
         companyName: 'Binance',
         position: 'Software Engineer',
         startDate: '2020-01-01',
         endDate: '2022-03-15',
       },
       {
+        type: 'work',
         companyName: 'Junction',
         position: 'Junio Software Developer',
         startDate: '2016-06-01',
