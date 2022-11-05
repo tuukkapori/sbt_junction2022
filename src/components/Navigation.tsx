3d76d283662804674e38640da58b1b86b898bc90
Filip Eller <filip.eller@aalto.fi>
2022-11-05 09:43:59 +0200

    Add minting to navbar.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
   AppBar,
   Avatar,
@@ -10,50 +10,50 @@ import {
   Autocomplete,
   Menu,
   MenuItem,
-} from '@mui/material'
-import { Outlet, useNavigate } from 'react-router-dom'
-import SearchIcon from '@mui/icons-material/Search'
-import { getUserByWalletId } from '../firebase'
-import { SettingsEthernet } from '@mui/icons-material'
+  Typography,
+} from '@mui/material';
+import { Outlet, useNavigate } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+import { getUserByWalletId } from '../firebase';
+import { SettingsEthernet } from '@mui/icons-material';
 
 const Navigation = ({ children }: any) => {
-  const [searchTerm, setSearchTerm] = useState('')
-  const [profileMenuAnchor, setProfileMenuAnchor] = useState<any>(null)
-  const [profileMenOpen, setProfileMenuOpen] = useState(false)
-  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('');
+  const [profileMenuAnchor, setProfileMenuAnchor] = useState<any>(null);
+  const [profileMenOpen, setProfileMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   const handleSearch = async () => {
-    console.log('search term ', searchTerm)
-    navigate(`profiles/${searchTerm}`)
-  }
+    console.log('search term ', searchTerm);
+    navigate(`profiles/${searchTerm}`);
+  };
 
   const handleOpenProfileMenu = (e: any) => {
-    setProfileMenuAnchor(e.currentTarget)
-    setProfileMenuOpen(true)
-  }
+    setProfileMenuAnchor(e.currentTarget);
+    setProfileMenuOpen(true);
+  };
 
   return (
     <div>
-      <AppBar position="sticky" sx={{ padding: 1, background: 'black' }}>
+      <AppBar position='sticky' sx={{ padding: 1, background: 'black' }}>
         <Box
           sx={{
             display: 'flex',
             justifyContent: 'space-between',
             padding: '0px 50px',
             alignItems: 'center',
-          }}
-        >
+          }}>
           <Box sx={{ display: 'flex' }}>
             <TextField
-              size="small"
-              variant="outlined"
+              size='small'
+              variant='outlined'
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={e => setSearchTerm(e.target.value)}
               sx={{ background: 'rgba(255, 255, 255, 0.9)' }}
-              placeholder="Enter wallet address"
+              placeholder='Enter wallet address'
               InputProps={{
                 startAdornment: (
-                  <InputAdornment position="start">
+                  <InputAdornment position='start'>
                     <IconButton>
                       <SearchIcon />
                     </IconButton>
@@ -64,18 +64,21 @@ const Navigation = ({ children }: any) => {
 
             <Button onClick={handleSearch}>Search</Button>
           </Box>
+
+          <MenuItem onClick={() => navigate('/send')}>
+            <Typography textAlign='center'>Mint</Typography>
+          </MenuItem>
           <IconButton onClick={handleOpenProfileMenu}>
             <Avatar />
           </IconButton>
           <Menu
-            id="basic-menu"
+            id='basic-menu'
             anchorEl={profileMenuAnchor}
             open={profileMenOpen}
             onClose={() => setProfileMenuOpen(false)}
             MenuListProps={{
               'aria-labelledby': 'basic-button',
-            }}
-          >
+            }}>
             <MenuItem onClick={() => navigate('profiles/me')}>
               My profile
             </MenuItem>
@@ -85,7 +88,7 @@ const Navigation = ({ children }: any) => {
       </AppBar>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
