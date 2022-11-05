4523e51638cf93fcd5b7a391585891a6eaa656da
Filip Eller <filip.eller@aalto.fi>
2022-11-05 11:59:28 +0200

    Fix welcome screen background again.

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -29,9 +29,6 @@ const WelcomeScreen = ({ currentWallet, setCurrentWallet, lmao }: any) => {
   return (
     <Box
       sx={{
-        background:
-          'linear-gradient( 105.3deg,  rgba(30,39,107,1) 21.8%, rgba(77,118,221,1) 100.2% );',
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
