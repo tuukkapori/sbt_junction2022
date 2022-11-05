cbda832dcc0ac54ec525d48a42fb0472fbef6080
Tuukka Pöri <tuukka.pori@aalto.fi>
2022-11-05 16:04:55 +0200

    Fix fails

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -47,19 +47,6 @@ const getUserByWalletId = async (walletId: string) => {
   }
 };
 
-const getCertificateById = async (certificateId: string) => {
-  const docRef = doc(db, 'certificates', certificateId);
-  const snap = await getDoc(docRef);
-
-  if (snap.exists()) {
-    console.log('snap exists ', snap.data());
-    return snap.data();
-  } else {
-    console.log('not founbd');
-    return undefined;
-  }
-};
-
 const getUsersBySearhTerm = async (search: string) => {
   if (search.startsWith('0x')) {
     const user = await getUserByWalletId(search);
