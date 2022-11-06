7119c71afdc5894f5b1fee631de7b2e20cca5252
Ahlström Kalle <hashtagkalski@gmail.com>
2022-11-06 03:51:35 +0200

    change for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as ReactDOMClient from 'react-dom/client'
 
 import App from './App'
-
+console.log("LGTM")
 const rootElement = document.getElementById('root')
 const root = ReactDOMClient.createRoot(rootElement)
 
